4fc6bf8c3df1d6a09fd5f99772594b087dd40b9a
Namitha Chandrashekara <namithaorama@gmail.com>
2017-05-03 17:18:27 +0200

    Client server changed according to Msg Type

--- src/main/scala-2.12/client.scala
+++ src/main/scala-2.12/client.scala
@@ -1,34 +1,46 @@
 /**
   * Created by adodh on 5/2/2017.
   */
-
-
 import java.net.{DatagramPacket, DatagramSocket, InetAddress}
+
+abstract sealed class MsgType
+case class Msg(message : String ) extends MsgType
+case class End () extends MsgType
 
 object client{
 
   def main(args : Array[String]): Unit = {
-    val udpSocket = new DatagramSocket(9898)
+
     println("Client is running...")
-    sendMsg("Hello Server", udpSocket)
-    receiveMsg(udpSocket)
+
+    sendOrRecClient(Msg("Hello Server"),"Sender")
+    sendOrRecClient(Msg(""),"Receiver")
+    sendOrRecClient(End(),"End")
   }
 
+  val udpSocket = new DatagramSocket(9898)
 
-  def sendMsg(message : String, udpSocket : DatagramSocket) {
-    val text = message.map(_.toByte).toArray
-    val packet = new DatagramPacket(text, text.length, InetAddress.getByName("localhost"), 9899)
-    udpSocket.send(packet)
+  def sendOrRecClient(msg:MsgType,senderOrReceiver:String):Unit = msg match{
+    case Msg(message:String) => {
+           if(senderOrReceiver  == "Sender"){
+              val text = message.map(_.toByte).toArray
+              val packet = new DatagramPacket(text, text.length, InetAddress.getByName("localhost"), 9899)
+              udpSocket.send(packet)
+            }
+            if(senderOrReceiver == "Receiver"){
+              val text = "........................".map(_.toByte).toArray
+              val packet = new DatagramPacket(text, text.length, InetAddress.getByName("localhost"), 9898)
+              udpSocket.receive(packet)
+              var msgReceived = new String(packet.getData())
+              println(msgReceived)
+              sendOrRecClient(Msg(msgReceived),"Sender")
+            }
+
+    }
+
+    case End() => {
+      udpSocket.close()
+    }
   }
 
-  def receiveMsg(udpSocket : DatagramSocket) {
-    val text = "........................".map(_.toByte).toArray
-    val packet = new DatagramPacket(text, text.length, InetAddress.getByName("localhost"), 9898)
-    udpSocket.receive(packet)
-    var msgReceived = new String(packet.getData())
-    println(msgReceived)
-    sendMsg(msgReceived, udpSocket)
-
-  }
 }
-
--- src/main/scala-2.12/server.scala
+++ src/main/scala-2.12/server.scala
@@ -4,31 +4,33 @@
 
 import java.net.{DatagramPacket, DatagramSocket, InetAddress}
 
-object server{
-  def main(args : Array[String]): Unit = {
-    val udpSocket = new DatagramSocket((9899))
+object server {
+  def main(args: Array[String]): Unit = {
+
     println("Server is running...")
-    receiveMsg(udpSocket)
+    sendOrRecServer(Msg(""), "Receiver")
   }
 
-  def receiveMsg(udpSocket: DatagramSocket) {
-    val text = ".............".map(_.toByte).toArray
-    val packet = new DatagramPacket(text, text.length, InetAddress.getByName("localhost"), 9899)
+  val udpSocket = new DatagramSocket(9899)
 
-    while(true) {
-      udpSocket.receive(packet)
-      var msgReceived = new String(packet.getData())
-      println(msgReceived)
-      sendMsg(msgReceived, udpSocket)
+  def sendOrRecServer(msg: MsgType, senderOrReceiver: String): Unit = msg match {
+    case Msg(message: String) => {
+      if (senderOrReceiver == "Receiver") {
+        val text = ".............".map(_.toByte).toArray
+        val packet = new DatagramPacket(text, text.length, InetAddress.getByName("localhost"), 9899)
 
-      //if end received then terminate (RETURN)
+        while (true) {
+          udpSocket.receive(packet)
+          var msgReceived = new String(packet.getData())
+          println(msgReceived)
+          sendOrRecServer(Msg(msgReceived), "Sender")
+        }
+      }
+      if (senderOrReceiver == "Sender") {
+        val text = (message + ":echoed").map(_.toByte).toArray
+        val reply = new DatagramPacket(text, text.length, InetAddress.getByName("localhost"), 9898)
+        udpSocket.send(reply)
+      }
     }
   }
-
-  def sendMsg(msgReceived : String, udpSocket : DatagramSocket): Unit ={
-    val text = (msgReceived + ":echoed").map(_.toByte).toArray
-    val reply = new DatagramPacket(text, text.length, InetAddress.getByName("localhost"), 9898)
-    udpSocket.send(reply)
-  }
-
 }
